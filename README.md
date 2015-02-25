32a926fd0010c520278f7e5a866fcd7c909cf734
Mark Bryant <mabryant02@gmail.com>
2015-02-25 12:01:20 -0500

    Add documenation and comments

---
+++ README.md
@@ -0,0 +1,10 @@
+Password Generator by Mark Bryant | mabryant02@gmail.com
+
+This is a simple encrypted string generator written in Ruby that may be suitable
+for creating passwords, depending on your security requirements.
+Run password-generator.rb if you have Ruby installed, or password-generator.exe
+if you are on Windows and do not have Ruby installed.
+
+If you need special or specific characters in the resulting password, include
+them in the 2 character salt. (e.g. !8 as salt will make !8 the first two
+characters of the resulting string).
--- password-generator.rb
+++ password-generator.rb
@@ -1,36 +1,37 @@
 #!/usr/bin/ruby
 #Password/encrypted string generator by Mark Bryant | mabryant02@gmail.com
 require 'rubygems' unless defined? Gem
-require 'clipboard'
+require 'clipboard' #clipboard gem is used for easy access to Windows clipboard.
 puts "Password Generator"
 def pw_gen
   puts "Enter the base string to be encrypted: "
-  base = gets.chomp
+  base = gets.chomp #This is the base string to be encrypted.
   puts "Enter salt characters ( 2 Characters only ): "
-  entry = gets.chomp
-  while entry.length != 2
-    if entry.length > 2
+  entry = gets.chomp #This is the 2 character salt the .crypt method requires.
+  while entry.length != 2 
+    if entry.length > 2 #if > 2 characters .crypt will only use first 2.
       puts "Entered more than 2 characters, using only the first 2."
       break
-    elsif entry.length < 2
+    elsif entry.length < 2 #script will error out if < 2 characters entered.
       puts "Salt must be at least 2 characters, please enter again: "
       entry = gets.chomp
     end
   end
-  result = base.crypt(entry)
+  result = base.crypt(entry) #store resulting encrypted string
   puts "Password is: #{result}"
-  puts "Should the password be copied to the clipboard? Y/N: "
+  puts "Copy password to the clipboard? Y/N: "
   copy = gets.chomp.downcase
   if copy == "y" || copy == "yes"
     Clipboard.copy(result)
     puts "Copied password to clipboard."
   end
   puts "Done? Y/N: "
-  $done = gets.chomp.downcase    
+  $done = gets.chomp.downcase #global variable to access outside of method.
+  														#there is probably a better way to do this.
 end
 pw_gen
 while true	
-  if $done == "y"
+  if $done == "y" || $done == "yes"
     break
   elsif 
     pw_gen
